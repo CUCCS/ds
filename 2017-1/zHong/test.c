6279cd7bbbed7204ea55abbe2cd171e894911044
钟总 <18811569079@163.com>
2017-03-19 00:33:14 +0800

    添加了对关键算法函数内部运行步骤的打印，去掉了main函数中不必要的说明

--- 2017-1/zHong/test.c
+++ 2017-1/zHong/test.c
@@ -21,31 +21,24 @@ void test2(ElemType *p){
 }
 
 int main(void){
-	printf("进入main函数\n\n");
 	LinkList La, Lb, Lc;
 	srand(time(0));
 
 	La = (LinkList)malloc(sizeof(LNode));
-	printf("开始创建单链表La\n");
 	CreateList_L(La, LEN+rand()%5, test1);
-	printf("创建完毕\n对单链表La进行遍历\n");
-	printf("La:");
-	TraverseList_L(La, print);
-	printf("\n遍历完毕\n\n");
-
 	Lb = (LinkList)malloc(sizeof(LNode));
-	printf("开始创建单链表Lb\n");
 	CreateList_L(Lb, LEN + rand() % 5, test2);
-	printf("创建完毕\n对单链表Lb进行遍历\n");
+
+	printf("\nLa:");
+	TraverseList_L(La, print);
+	printf("\n");
 	printf("Lb:");
 	TraverseList_L(Lb, print);
-	printf("\n遍历完毕\n\n");
+	printf("\n\n");
 
-	printf("开始归并单链表La与Lb\n");
 	MergeList_L(La, Lb, &Lc);
-	printf("归并完毕\n对单链表Lc进行遍历\n");
 	printf("Lc:");
 	TraverseList_L(Lc, print);
-	printf("\n遍历完毕\n\n");
+	printf("\n");
 	return 0;
 }
