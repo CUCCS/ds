d2f390331ce8a68eae1804c1437fb30e0906be3b
jackcily <3417934244@qq.com>
2017-03-20 22:39:18 +0800

    第三次提交 作业 2.12
    第三次提交 作业 2.12

--- hello.cpp
+++ hello.cpp
@@ -1,27 +1,33 @@
-﻿#include"hello.h"
-
+﻿#include "hello.h"
 int main()
 {
 	srand(time(0));
 	LinkList la, lb, lc;
-	int a=4, b=4;//la的长度和lb的长度均为4
+	int a = LIST_A_NUM;
+	int b = LIST_B_NUM;//la的长度和lb的长度均为宏常量
 	CreatList(la, a);
-	output(la);
+
+	output(la,a);
 	CreatList(lb, b);
-	output(lb);
+	
+	output(lb,b);
 	MergeList(la, lb, lc);
-	free(lb);
-	lc = lc->next;
+	if (lb != NULL)
+	{
+      free(lb);
+	}
+	
 	output(lc, a + b);
 	return 0;
 
 }
+//逆序输入n个元素的值，建立单链表
 void CreatList(LinkList &L, int n)
 {
-	//逆序输入n个元素的值，建立单链表
+	
 	int i;
-	int a[4];
-	assign_value(a);
+	int *a=(int*)malloc(sizeof(int)*n);
+	assign_value(a,n);
 	LinkList p;
 	L = (LinkList)malloc(sizeof(LNode));//头节点
 	L->next = NULL;
@@ -33,13 +39,15 @@ void CreatList(LinkList &L, int n)
 	}
 
 }
+//已知单链表线性表la和lb的元素按值非递减排列
+	//合并后的链lc 仍然按值非递减排列
 void MergeList(LinkList&la, LinkList&lb, LinkList&lc)
 {
-	//已知单链表线性表la和lb的元素按值非递减排列
-	//合并后的链lc 仍然按值非递减排列
-	LinkList  pa, pb, pc, l = NULL;
+	int  j, i=1,k1,k2;//打印时用于输出计数
+	LinkList  pa, pb, pc, l = NULL,lc_copy;
 	pa = la->next;
 	pb = lb->next;
+	
 	lc = pc = la;
 	while (pa&&pb)
 	{
@@ -55,55 +63,66 @@ void MergeList(LinkList&la, LinkList&lb, LinkList&lc)
 			pc = pb;
 			pb = pb->next;
 		}
+		lc_copy = lc->next ;
+		for (j = 0; j < i; j++)
+		{
+		
+			printf("%d ", lc_copy->data);
+			lc_copy=lc_copy->next;
+		}
+		printf("\n");
+		i++;
 		//厉害了  我的算法；
 	}
 	pc->next = pa ? pa : pb;
-	
-}
-void assign_value(int a[])
-{
-	int i ,j,temp ;
-	//产生随机数
-	for (i = 0; i < 4; i++)
+	lc_copy = lc->next;
+	k1 = LIST_A_NUM;
+	k2 = LIST_B_NUM;
+	k1 = k1 + k2;
+	for (i = 0; i <k1; i++)
 	{
-		a[i]=rand() % 20 + 1;
+		printf("%d ", lc_copy->data);
+		lc_copy = lc_copy->next;
 	}
-   // 冒泡法对随机数排序
-	for (i = 0; i < 3; i++)
+	printf("\n");
+}
+//产生随机数，并进行排序
+void assign_value(int a[],int n)
+{
+	int i, j, temp;
+	//产生随机数
+	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < 3- i; j++)
+		a[i] = rand() % 20 + 1;
+	}
+	// 冒泡法对随机数排序
+	for (i = 0; i < n-1; i++)
+	{
+		for (j = 0; j < n-1 - i; j++)
 		{
 			if (a[j] < a[j + 1])
 			{
 				temp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = temp;
-				
+
 			}
 		}
-  }
+	}
 
 
 }
+//输出链表
 void output(LinkList  l, int n)
 {
 	int i;
 	printf("\nthe List is:\n");
+    l = l->next;
 	for (i = n; i > 0; i--)
 	{
 		printf("%d  ", l->data);
 		l = l->next;
 
 	}
-}
-void output(LinkList l)
-{
-	int i;
-	l = l->next;
-	printf("\nthe List:\n");
-	for (i = 0; i < 4; i++)
-	{
-		printf("%d  ", l->data);
-		l = l->next;
-	}
+	printf("\n\n");
 }
--- hello.h
+++ hello.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define LIST_A_NUM 4;
+#define LIST_B_NUM 4;
 typedef struct LNode
 {
 	int data;
@@ -9,6 +11,5 @@ typedef struct LNode
 }LNode, *LinkList;
 void CreatList(LinkList &L, int n);
 void MergeList(LinkList&la, LinkList&lb, LinkList&c);
-void output(LinkList l, int n);// 输出合并后的链表，验证是否正确合并
-void output(LinkList l);//输出合并前的链表
-void assign_value(int a[]);//随机给数组赋值
+void output(LinkList l, int n);
+void assign_value(int a[],int n);//随机给数组赋值
