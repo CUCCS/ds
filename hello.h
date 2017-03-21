612a5d906df737d3b71a725d872085cb98f84594
jackcily <3417934244@qq.com>
2017-03-21 12:45:33 +0800

    第四次提交   作业2.12
    第四次提交   作业2.12

--- hello.h
+++ hello.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define LIST_A_NUM 4;
-#define LIST_B_NUM 4;
+#define  x 5;
+#define y 4;
 typedef struct LNode
 {
 	int data;
@@ -11,5 +11,8 @@ typedef struct LNode
 }LNode, *LinkList;
 void CreatList(LinkList &L, int n);
 void MergeList(LinkList&la, LinkList&lb, LinkList&c);
-void output(LinkList l, int n);
-void assign_value(int a[],int n);//随机给数组赋值
+void output(LinkList l);
+void assign_value(int a[], int n);
+
+
+
