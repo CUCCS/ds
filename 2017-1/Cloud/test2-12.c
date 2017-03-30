0cc62f6f30ab40a79bc0c2fdaf56356ffea0aa11
Cloud0606 <827594950@qq.com>
2017-03-30 10:45:37 +0800

    2-12(3)

--- 2017-1/Cloud/test2-12.c
+++ 2017-1/Cloud/test2-12.c
@@ -1,93 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
-#define dataIncreasBase 6; 
-
-struct _LNode{
-	int data;
-	struct _LNode *next ;
-}LNode,*LinkList;
-
-typedef  struct _LNode LNode;
-typedef  struct _LNode* LinkList;
-
-void Traverse(LinkList p);
-void CreatList(LinkList L,int n);
-void MergeList( LinkList La, LinkList Lb, LinkList Lc );
-
-void Traverse(LinkList p){
-	p = p->next ;
-	printf("有序单链表为:");
-	while(p->data){
-		printf("%d ",p->data );
-		p = p->next ;
-	}
-	printf("\n");
-
-}
-void CreatList(LinkList L,int n){
-
-	srand( time(NULL) );
-	LNode *p;//新生成的节点
-	int lastData;//储存上个节点的数据
-	int i;
-	lastData = rand()%dataIncreasBase;
-	L = (LinkList)malloc( sizeof(LNode) );
-	L->next = NULL;//建立单链表的头结点
-	/*if(L){
-		printf("头节点创建成功！\n");
-	}*/
-	for( i = 0; i < n ;i++){
-		p = (LinkList)malloc(sizeof(LNode));
-		while( !p ){
-			p = (LinkList)malloc(sizeof(LNode));
-		}
-		p->data = lastData;
-		lastData += rand()%dataIncreasBase;//随机生成>=lastdata的数据
-		p->next  = L->next ;//第一次连入时L->next，第一次插入的节点在整个链表的最后。
-		L->next  = p;//两步把一个新生成的结点连上去了
-	}
-	Traverse(L);
-}
-void MergeList( LinkList La, LinkList Lb, LinkList Lc ){
-	
-	LinkList pa,pb,pc;
-	pa = La->next ;
-	pb = Lb->next ;
-	Lc = pc = La;
-	//pa pb都未next到最后一个节点
-	while(pa && pb){
-		if( pa->data <= pb->data ){
-			pc->next = pa;
-			pc = pa;
-			pa = pa->next ;
-		}
-		else{
-			pc->next  = pb;
-			pc = pb;
-			pb = pc->next;
-		}
-	}
-	pc->next = pa ? pa : pb;  
-	//	free( pa ? pb : pa);
-}
-int main(){
-
-	//srand( time(NULL) );
-	LinkList list_a, list_b, list_c;
-    int length = 10;
-	CreatList(list_a,length);
-	Traverse(list_a );
-	CreatList(list_b,length);
-	Traverse(list_b );
-	MergeList(list_a,list_b,list_c);
-	Traverse(list_c );
-	return 0;
-}
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#define dataIncreasBase 6; 
+#define length 10
 
 typedef  struct LNode{
 	int data;
@@ -100,49 +13,51 @@ typedef enum {
 	false
 }bool;
 
-void Traverse(LinkList p);
+void travle(LinkList p);
 void CreatList(LinkList L,int n);
-void MergeList( LinkList La, LinkList Lb, LinkList Lc );
+void MergeList(LinkList La,LinkList Lb,LinkList Lc);
 
-void Traverse(LinkList p){
-	p = p->next ;
-	printf("有序单链表为:");
-	while(p->data){
+void travle(LinkList p){
+
+	int i;
+	printf("有序单链表:");
+	//p = L_->next
+	for( i = 0 ;i <length;i++){
+		//printf("%d ",(L_+i)->data);
 		printf("%d ",p->data );
 		p = p->next ;
 	}
 	printf("\n");
-
 }
+
 void CreatList(LinkList L,int n){
-	LNode *p;//新生成的节点
-	int lastData;//储存上个节点的数据
+	LNode *p;
 	int i;
-	lastData = rand()%dataIncreasBase;
 	L = (LinkList)malloc( sizeof(LNode) );
 	L->next = NULL;//建立单链表的头结点
-	/*if(L){
+	if(L)
 		printf("头节点创建成功！\n");
-	}*/
-	for( i = 0; i < n ;i++){
+	for(i = n; i > 0;--i){
 		p = (LinkList)malloc(sizeof(LNode));
-		while( !p ){
-			p = (LinkList)malloc(sizeof(LNode));
-		}
-		p->data = lastData;
-		lastData += rand()%dataIncreasBase;//随机生成>=lastdata的数据
-		p->next  = L->next ;//第一次连入时L->next，第一次插入的节点在整个链表的最后。
+		if(p)
+			printf("成功分配一个节点空间，请输入一个整数：");
+		scanf("%d",& p->data );
+		p->next  = L->next ;//第一次连入时L->next，第一次插入的节点在整个链表的最后。？
 		L->next  = p;//两步把一个新生成的结点连上去了
 	}
-	Traverse(L);
+	travle(L);
+	//printf("生成一个有序单链表");
+	//for(int i ;i <length;i++){
+	//	printf("%d",L+i);
+	//}
+
 }
-void MergeList( LinkList La, LinkList Lb, LinkList Lc ){
-	
+
+void MergeList(LinkList La,LinkList Lb,LinkList Lc){
 	LinkList pa,pb,pc;
 	pa = La->next ;
 	pb = Lb->next ;
 	Lc = pc = La;
-	//pa pb都未next到最后一个节点
 	while(pa && pb){
 		if( pa->data <= pb->data ){
 			pc->next = pa;
@@ -156,19 +71,17 @@ void MergeList( LinkList La, LinkList Lb, LinkList Lc ){
 		}
 	}
 	pc->next = pa ? pa : pb;  
-	//	free( pa ? pb : pa);
+	free(Lb);
 }
-int main(){
 
-	srand( time(0) );
-	LNode list_a, list_b, list_c;
-    int length = 10;
+int main(){
+	LNode list_a,list_b,list_c;
+
 	CreatList(&list_a,length);
-	Traverse(&list_a );
 	CreatList(&list_b,length);
-	Traverse(&list_b );
 	MergeList(&list_a,&list_b,&list_c);
-	Traverse(&list_c );
+	travle(&list_c);
 	return 0;
 }
+
 
