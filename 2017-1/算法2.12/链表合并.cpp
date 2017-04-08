5470dfe163b903d0eed8baff52868d7daeacbca3
HuanWoWeiLan <962871150@qq.com>
2017-04-08 12:59:12 +0800

    2.12算法第二次提交

--- 2017-1/算法2.12/链表合并.cpp
+++ 2017-1/算法2.12/链表合并.cpp
@@ -1,84 +1,179 @@
-#include<stdio.h>
-#include <stdlib.h>
-#define MAXSIZE 1000
+//#include<stdio.h>
+//#include<time.h>
+//#include <stdlib.h>
+//#define MAXSIZE 1000
+//
+//typedef struct LNode{
+//	int data;
+//	struct LNode *next;
+//}LNode,*LinkList;
+//LNode  *Create()  //返回值: 链表的首指针
+//{
+//
+//}
+//LinkList MergeList_L(LinkList La,LinkList Lb)//合并
+//{
+//	LinkList   pa,pb,pc,Lc; 
+//	Lc=(LinkList)malloc(sizeof(LNode)); 
+//	Lc-> data=La-> data+Lb-> data; 
+//	pa=La-> next; 
+//	pb=Lb-> next; 
+//	Lc=pc=La; 
+//	while(pa&&pb) 
+//	{ 
+//		if(pa-> data <=pb-> data) 
+//		{ 
+//			pc-> next=pa;pc=pa;pa=pa->next; 
+//		} 
+//		else 
+//		{pc-> next=pb;pc=pb;pb=pb-> next;} 
+//	} 
+//	pc-> next=pa?pa:pb; 
+//	free(Lb); 
+//	return   Lc; 
+//} 
+//int PrintLinkList(LinkList L) //打印链表 
+//{ 
+//	LinkList   q; 
+//	q=L; 
+//	if(q!=NULL) 
+//	{
+//		q=q-> next; 
+//		do 
+//		{ 
+//			printf( "%d ",q-> data); 
+//			q=q-> next; 
+//		} 
+//		while(q!=NULL); 
+//		printf( "\n "); 
+//	} 
+//	return 0; 
+//} 
+//int main()
+//{
+//	LinkList L1=Create();
+//	LinkList L2=Create();
+//	LinkList L3;
+//	L3=MergeList_L(L1,L2);                           
+//	PrintLinkList(L3);  
+//
+//	return 0;
+//
+//
+//}
 
-typedef struct LNODE{
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+#include<malloc.h>
+
+typedef struct LNode{
+
 	int data;
-	struct LNODE *next;
-}LNODE,*LinkList;
-LNODE  *Create()  //返回值: 链表的首指针
+
+	struct LNode *next;
+
+}LNode,*LinkList;
+
+void CreateList(LinkList &L,int n)
+
 {
-    LNODE  *p1, *p2, *head;    int a;   head = NULL;
-    //输入一个整数, 以 －1 结束: 
-    scanf("%d",&a);
-    while( a != -1 ) // 循环输入数据，建立链表
-    {
-        p1=(LNODE *)malloc(sizeof(LNODE));
-        p1->data = a;
-        if(head==0 ) // ①建立首结点
-        {
-            head=p1;
-            p2=p1;
-        }
-        else // ②处理中间结点
-        {
-            p2->next=p1;
-            p2=p1;
-        }
-        printf("请输入一个整数, 以 －1 结束: ");
-        scanf("%d",&a);
-    }
-    if(head != 0)
-        p2->next=0;//③处理尾结点, 可能第1次就输入 -1
-    return(head); //返回创建链表的首指针
+
+	LinkList p;
+
+	L=(LinkList)malloc(sizeof(LNode));
+
+	L->next=NULL;//先初始为空
+
+	printf("请按顺序输入链表的值\n");
+
+	srand((unsigned) time(NULL));
+
+	for(int i=n;i>0;--i)
+
+	{
+
+		p=(LinkList)malloc(sizeof(LNode));
+
+		p->data=rand()%101;
+
+		printf("%d ",&p->data);
+
+		p->next=L->next;
+
+		L->next=p;
+
+	}
+
 }
+
+void PrintList(LinkList L)
+
+{
+
+	LinkList p;
+
+	p=L->next;
+
+	while(p)
+
+	{
+
+		printf("%d ",p->data);
+
+		p=p->next;
+
+	}
+
+	printf("\n");
+
+}
+
 LinkList MergeList_L(LinkList La,LinkList Lb)//合并
 {
-  LinkList   pa,pb,pc,Lc; 
-  Lc=(LinkList)malloc(sizeof(LNODE)); 
-  Lc-> data=La-> data+Lb-> data; 
-  pa=La-> next; 
-  pb=Lb-> next; 
-  Lc=pc=La; 
-  while(pa&&pb) 
-    { 
-      if(pa-> data <=pb-> data) 
-        { 
-          pc-> next=pa;pc=pa;pa=pa->next; 
-        } 
-      else 
-        {pc-> next=pb;pc=pb;pb=pb-> next;} 
-    } 
-    pc-> next=pa?pa:pb; 
-    free(Lb); 
-    return   Lc; 
+	LinkList   pa,pb,pc,Lc; 
+	Lc=(LinkList)malloc(sizeof(LNode)); 
+	Lc-> data=La-> data+Lb-> data; 
+	pa=La-> next; 
+	pb=Lb-> next; 
+	Lc=pc=La; 
+	while(pa&&pb) 
+	{ 
+		if(pa-> data <=pb-> data) 
+		{ 
+			pc-> next=pa;pc=pa;pa=pa->next; 
+		} 
+		else 
+		{pc-> next=pb;pc=pb;pb=pb-> next;} 
+	} 
+	pc-> next=pa?pa:pb; 
+	free(Lb); 
+	return   Lc; 
 } 
-int PrintLinkList(LinkList L) //打印链表 
-{ 
-  LinkList   q; 
-  q=L; 
-  if(q!=NULL) 
-  {
-    q=q-> next; 
-    do 
-      { 
-        printf( "%d ",q-> data); 
-                q=q-> next; 
-      } 
-    while(q!=NULL); 
-  printf( "\n "); 
-  } 
-  return 0; 
-} 
-int main()
+void main()
+
 {
-	LinkList L1=Create();
-	LinkList L2=Create();
-	LinkList L3;
-	L3=MergeList_L(L1,L2);                           
-    PrintLinkList(L3);  
 
-	return 0;
+	LinkList La,Lb,Lc;
+	int length1;
+	int length2;
+	printf("链表La的长度为：");
+	scanf("%d",&length1);
 
+	printf("链表La的输入为：");
+	CreateList(La,length1);
 
-}
+	printf("链表Lb的长度为：");
+	scanf("%d",&length2);
+
+	printf("链表Lb的输入为：");
+	CreateList(Lb,length2);
+
+	 Lc=MergeList_L(La,Lb);
+
+	printf("链表Lc的合并输出为：");
+
+	PrintList(Lc);
+
+} 
