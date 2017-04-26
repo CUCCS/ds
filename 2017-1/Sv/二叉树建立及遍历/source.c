898ca686d3651b5a94a026aec1f05611108f2949
DcmTruman <975678774@qq.com>
2017-04-26 17:19:58 +0800

    增加新的测试用例

--- 2017-1/Sv/二叉树建立及遍历/source.c
+++ 2017-1/Sv/二叉树建立及遍历/source.c
@@ -3,7 +3,8 @@ int main(){
     char *p=(char*)malloc(100*sizeof(char));
 	//scanf("%s",p);
 	//用0代替空树结点
-	p = "ABDG000EH00I0K00C0F00";
+	//p = "ABDG000EH00I0K00C0F00";
+        p = "ABD0F000CE000";
 	printf("测试字符串:\n%s\n", p);
 	BiTNode *T=NULL;
 	printf("后序序列：\n");
