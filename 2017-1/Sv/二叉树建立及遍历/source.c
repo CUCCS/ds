98c509a223a885ac199d1238ccfcb97cd9f9e6d0
DcmTruman <975678774@qq.com>
2017-04-26 16:54:26 +0800

    二叉树建立及后序遍历

---
+++ 2017-1/Sv/二叉树建立及遍历/source.c
@@ -0,0 +1,12 @@
+﻿#include"Tree.h"
+int main(){
+    char *p=(char*)malloc(100*sizeof(char));
+	//scanf("%s",p);
+	//用0代替空树结点
+	p = "ABDG000EH00I0K00C0F00";
+	printf("测试字符串:\n%s\n", p);
+	BiTNode *T=NULL;
+	printf("后序序列：\n");
+	T=CreateBiTree(T,p);
+	Preorder(T);
+}
