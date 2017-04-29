61adcb9b44f66ed42e1ba3eb3176cdf84d41bea0
Loxjan <lawliet@loxjan.com>
2017-04-29 13:58:31 +0800

    修改括号匹配

--- 2017-1/Micylt/hw2/hw2-2.c
+++ 2017-1/Micylt/hw2/hw2-2.c
@@ -1,135 +1,91 @@
-//括号匹配程序
+//括号匹配的检验程序
 #include <stdio.h>
 #include <stdlib.h>
-#define STACKINCREAMENT 10
-#define STACK_INIT_SIZE 100
-#define OVERFLOW -2
-#define OK 1
-#define ERROR 0
-typedef int status;
-typedef char SElemtype;
-
-typedef struct
+#include <string.h>
+#define  MAXLEN 50
+typedef struct stack
 {
-	SElemtype *base;
-	SElemtype *top;
-	status stacksize;
-}sqstack;
-
-//初始化栈
-status Init(sqstack *s)
+	char ch[50];
+	int top;
+}ST;
+//栈的初始化
+ST *ST_Init()
 {
-	s->base = (SElemtype *)malloc(STACK_INIT_SIZE * sizeof(SElemtype));
-	if (!s->base)
-		exit(OVERFLOW);
-	s->top = s->base;
-	s->stacksize = STACK_INIT_SIZE;
-	return OK;
-}
-
-//获取栈顶元素
-status Gettop(sqstack *s, SElemtype e)
-{
-	if (s->top == s->base)
-		return ERROR;
-	e = *(s->top - 1);
-	return OK;
-}
-
-//压栈
-status push(sqstack *s, SElemtype e)
-{
-	if (s->top - s->base >= s->stacksize)
+	ST *st;
+	if (st = (ST *)malloc(sizeof(ST)))
 	{
-		s->base = (SElemtype *)realloc(s->base, (s->stacksize + STACKINCREAMENT) * sizeof(SElemtype));
-		if (!s->base)
-			exit(OVERFLOW);
-		s->top = s->base + s->stacksize;
-		s->stacksize += STACKINCREAMENT;
+		st->top = 0;
+		return st;
 	}
-	*s->top++ = e;
-	return OK;
+	return NULL;
 }
-
-//出栈
-status pop(sqstack *s, SElemtype *e)
+//出栈操作
+int Pop(ST *st)
 {
-	if (s->top == s->base)
-		return ERROR;
-	*e = *--s->top;
-	return OK;
-}
-
-//判断是否为空栈
-status stackempty(sqstack *s)
-{
-	if (s->top == s->base)
-		return OK;
-	return ERROR;
-}
-
-//清空栈
-status clearstack(sqstack *s)
-{
-	if (s->top == s->base)
-		return ERROR;
-	s->top = s->base;
-	return OK;
-}
-
-//括号匹配算法
-status Parenthesis_match(sqstack *s, char *str)
-{
-	int i = 0, flag = 0;
-	SElemtype e;
-	while (str[i] != '\0')
+	if (st->top == 0)
 	{
-		switch (str[i])
-		{
-		case '(':
-			push(s, str[i]);
-			break;
-		case '[':
-			push(s, str[i]);
-			break;
-		case ')':
-		{
-			pop(s, &e);
-			if (e != '(')
-				flag = 1;
-		}
-		break;
-		case ']':
-		{
-			pop(s, &e);
-			if (e != '[')
-				flag = 1;
-		}
-		break;
-		default:
-			break;
-		}
-		if (flag)
-			break;
-		i++;
+		printf("栈为空\n");
+		return 0;
 	}
+	st->top--;
+	return st->ch[st->top];
+}
 
-	if (!flag && stackempty(s))
-		printf("括号匹配成功!\n");
+//入栈操作
+void Push(ST *st, char c)
+{
+	if (st->top == MAXLEN)
+	{
+		printf("栈溢出\n");
+		return;
+	}
+	st->ch[st->top] = c;
+	st->top++;
+}
+//符号检验函数
+void check_symbol(ST *st, char *a)
+{
+	int i;
+	Push(st, a[0]);
+
+	for (i = 1; i< strlen(a); i++)
+	{
+		//遍历每一个元素，判断出栈还是入栈
+		if ((a[i] == ']'&&st->ch[st->top - 1] == '[') || (a[i] == ')'&&st->ch[st->top - 1] == '('))//出栈的条件
+		{
+			Pop(st);
+		}
+		else
+		{
+			Push(st, a[i]);
+		}
+	}
+	if (st->top == 0)
+	{
+		printf("括号是匹配的\n\n");
+	}
 	else
-		printf("括号匹配失败!\n");
-	return OK;
+
+	{
+		printf("括号不匹配\n\n");
+	}
 }
 
-int main()
+void main()
 {
-	char str[100], enter;
-	sqstack s;
-	Init(&s);
-	printf("请输入括号串:\n");
-	scanf("%s", str);
-	scanf("%c", &enter);
-	Parenthesis_match(&s, str);
-	return 0;
-}
 
+	while (1)
+	{
+		char s[50];
+		ST *st;
+		st = ST_Init();
+		printf("请输入一串括号||输入‘e’退出程序：\n");
+		scanf("%s", s);
+		if (s[0] == 'e')
+		{
+			return ;
+		}
+		check_symbol(st, s);
+	}
+
+}
