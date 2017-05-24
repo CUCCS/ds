2e9c54ee3fe73245965f1bc1c08e7b88338dbea6
钟总 <18811569079@163.com>
2017-05-24 23:03:46 +0800

    【B树】插入算法√

---
+++ 2017-1/zkz/Tree3/Tree/Btest.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include "BTree.h"
+
+void Traverse(Node* tree) {
+	for (int i = 0; i < M; ++i) {
+		//childs
+		if (tree->childs[i])Traverse(tree->childs[i]);
+		if (i == M - 1)break;
+		//keys
+		if(tree->keys[i])printf("%d ",tree->keys[i]);
+	}
+}
+int main() {
+	BTree tree = newBTree();
+	int inputs[] = { 8, 10, 14, 3, 1, 6, 4, 7, 5, 19, 22, 30 };
+	int size1 = 12;
+	for (int i = 0; i < size1; ++i) {
+		tree = Insert(tree, inputs[i]);
+	}
+	Traverse(tree);
+}
