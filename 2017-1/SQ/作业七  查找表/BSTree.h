864c2326e64f3182e8ae9b60a88a3245092f11f5
Lycan_J <jckling@163.com>
2017-05-24 21:35:46 +0800

    实现二叉排序树的查找等操作

---
+++ 2017-1/SQ/作业七  查找表/BSTree.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int KeyType; // 关键字类型
+typedef int ElemType;
+
+typedef enum { // 函数返回值
+	ERROR,
+	OK
+}Status;
+
+typedef enum { // 定义布尔函数值
+	false,
+	true
+}bool;
+
+typedef struct BSTNode { // 结点结构
+	KeyType key;
+	struct BSTNode  *lchild, *rchild; // 左右孩子指针
+} BSTNode, *BSTree;
+
+Status CreateBST(BSTree *T, ElemType d[], int n);// 构造一棵二叉排序树
+Status SearchBST(BSTree T, KeyType kval, BSTree f, BSTree *p);// 在树中寻找关键字为kval的元素
+Status InsertBST(BSTree *T, ElemType e);// 向树中插入元素
+bool EQ(KeyType a, KeyType b);// 相等判断
+bool LT(KeyType a, KeyType b);// 小于判断
+Status DeleteBST(BSTree *T, KeyType kval);// 删除树中关键字为kval的元素
+Status Delete(BSTree *p);// 删除的具体操作
+Status PreOrderTraverse(BSTree T, Status(*Visit)(ElemType));// 先序遍历
+Status Visit(ElemType e);// 输出树中的元素
+Status PrintTree(BSTree T);// 输出整棵树
---
+++ 2017-1/SQ/作业七  查找表/Test.c
@@ -0,0 +1,33 @@
+#include "BSTree.h"
+
+int main(){
+	ElemType test1[] = { 8,10,14,3,1,6,4,7,5,19,22,30 };
+	KeyType Key[] = { 13,8,5,20,6 };
+	int elem_number, search_number;
+	int i;
+
+	elem_number = sizeof(test1) / sizeof(ElemType);
+	search_number = sizeof(Key) / sizeof(KeyType);
+
+	BSTree Tree = NULL;
+	BSTree temp = NULL;
+	FILE *p;
+
+	p = fopen("D:\\BaiduYunDownload\\BSTOutput.txt", "w+");
+
+	CreateBST(&Tree, test1, elem_number);
+	PrintTree(Tree,p);
+
+	for (i = 0; i < search_number; i++) {
+		if (SearchBST(&Tree, Key[i], NULL, &temp)) {
+			DeleteBST(&Tree, Key[i]);
+			PrintTree(Tree,p);
+		}
+		else {
+			InsertBST(&Tree, Key[i]);
+			PrintTree(Tree,p);
+		}
+	}
+
+	return 0;
+}
