3c87ff4412dc3416079d6dafd5e69224b47a207e
钟总 <18811569079@163.com>
2017-05-24 12:55:31 +0800

    【二叉搜索树+二叉平衡树】代码（已分离头文件）√

--- 2017-1/zkz/Tree3/Tree/test.c
+++ 2017-1/zkz/Tree3/Tree/test.c
@@ -20,11 +20,25 @@ int main() {
 	int size2 = 5;
 
 	/*初始化查找表*/
-	AVL avl = newAVL(8);
+	BST bst = newBST(inputs[0]);
+	AVL avl = newAVL(inputs[0]);
 	for (int i = 1; i < size1; ++i) {
+		bst = BSTinsert(bst, inputs[i]);
 		avl = AVLinsert(avl, inputs[i]);
 		if (DEBUG)SuperShow(avl),printf("\n");
 	}
+	printf("BST:\n");
+	Show(bst);
+	printf("\n");
+	/*开始查找操作*/
+	for (int j = 0; j < size2; ++j) {
+		/*如果找到就删除，没找到就插入*/
+		BSTfind(bst, toFind[j]) ? BSTremove(bst, toFind[j]) : BSTinsert(bst, toFind[j]);
+		Show(bst);
+		printf("\n");
+	}
+
+	printf("AVL:\n");
 	Show(avl);
 	printf("\n");
 	/*开始查找操作*/
