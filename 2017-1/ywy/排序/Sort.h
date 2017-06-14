1a1effaf69020d346786b8a0d01c490af027a2d7
yang5220 <3425991052@qq.com>
2017-06-14 13:50:58 +0800

    排序

---
+++ 2017-1/ywy/排序/Sort.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+typedef int KeyType;
+typedef int InfoType;
+#define maxsize 20
+#define len 10
+#define LT(a,b) ((a)<(b))
+typedef struct 
+{
+	KeyType key;
+	InfoType otherinfo;
+}RedType;
+typedef struct
+{
+	RedType r[maxsize+1];//r[0]×÷ÎªÔÝ´æ¿Õ¼ä
+	int length;
+}SqList;
+void swap(SqList*L, int i, int j);
+void ShellSort(SqList*L);
+void InsertSort(SqList*L);
+void BubbleSort(SqList*L);
+void QuickSort(SqList*L);
+void QSort(SqList*L, int low, int high, int*count1, int*count2);
+int Partition(SqList*L, int low, int high,int*count1,int*count2);
+void SelectSort(SqList*L);
---
+++ 2017-1/ywy/排序/test.c
@@ -0,0 +1,26 @@
+#include"Sort.h"
+//#include"Sort.c"
+int main()
+{
+	SqList L, L1, L2, L3, L4;
+	L.length = len;
+	int i = 0;
+
+	for (i = 1; i <= len; i++)
+	{
+		L.r[i].key = 10 - i;;
+	}
+	printf("\nÔ­Ë³ÐòÎª£º");
+	for (i = 1; i<L.length; i++)
+	{
+		printf("%d ", L.r[i].key);
+	}
+	printf("\n\n");
+	L1 = L2 = L3 = L4 = L;
+	BubbleSort(&L);
+	InsertSort(&L1);
+	QuickSort(&L2);
+	SelectSort(&L3);
+	ShellSort(&L4);
+	return 0;
+}
